5437f00a2a35339ec7ce1fff20f10ef5e6736ca0
dogmania <hj296914@gmail.com>
2025-10-19 22:39:44 +0900

    :sparkles: Feat: Koin, Ktor, Serialization 라이브러리 의존성 추가

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,10 +1,32 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import com.codingfeline.buildkonfig.compiler.FieldSpec.Type
+import com.codingfeline.buildkonfig.gradle.BuildKonfigExtension
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.buildKonfig)
+}
+
+extensions.configure<BuildKonfigExtension> {
+    packageName = "com.illdan.desktop"
+
+    val props = Properties().apply {
+        val file = rootProject.file("local.properties")
+        if (file.exists()) file.inputStream().use { load(it) }
+    }
+    val baseUrl = props.getProperty("BASE_URL")
+
+    defaultConfigs {
+        buildConfigField(
+            Type.STRING,
+            "BASE_URL",
+            baseUrl
+        )
+    }
 }
 
 kotlin {
@@ -18,8 +40,20 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.lifecycleViewModel)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            implementation(libs.ktor.client)
+            implementation(libs.ktor.serialization)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.client.logging)
+
+            implementation(libs.koinCore)
+            implementation(libs.koinCompose)
+            implementation(libs.koinComposeViewModel)
+
+            implementation(libs.serialization)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -27,6 +61,7 @@ kotlin {
         jvmMain.dependencies {
             implementation(compose.desktop.currentOs)
             implementation(libs.kotlinx.coroutinesSwing)
+            implementation(libs.ktor.client.okhttp)
         }
     }
 }
