313d1a807d2b4071bcc1ab56ff74c5b36cb7d6d1
dogmania <hj296914@gmail.com>
2025-10-20 18:07:26 +0900

    ✨ Feat: repositoryModule, mainModule 추가

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/app/di/InitKoin.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/app/di/InitKoin.kt
@@ -1,12 +1,17 @@
 package com.illdan.desktop.app.di
 
+import com.illdan.desktop.core.network.di.networkModule
+import com.illdan.desktop.data.di.repositoryModule
+import com.illdan.desktop.presentation.main.di.mainModule
 import org.koin.core.context.startKoin
 
 fun initKoin() {
     startKoin {
         modules(
             listOf(
-                networkModule
+                networkModule,
+                repositoryModule,
+                mainModule
             )
         )
     }
