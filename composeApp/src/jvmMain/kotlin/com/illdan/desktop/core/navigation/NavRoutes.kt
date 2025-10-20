3483cc54161f212ece58834ed17da5dc93d52a06
dogmania <hj296914@gmail.com>
2025-10-20 17:59:49 +0900

    ✨ Feat: NavRoutes 구현

---
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/core/navigation/NavRoutes.kt
@@ -0,0 +1,12 @@
+package com.illdan.desktop.core.navigation
+
+sealed class NavRoutes(val route: String) {
+
+    // Main
+    data object MainGraph: NavRoutes("main/graph")
+    data object MainScreen: NavRoutes("main/screen")
+
+    // Splash
+    data object SplashGraph: NavRoutes("splash/graph")
+    data object SplashScreen: NavRoutes("splash/screen")
+}
