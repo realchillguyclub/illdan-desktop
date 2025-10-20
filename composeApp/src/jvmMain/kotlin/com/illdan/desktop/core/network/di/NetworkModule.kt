49efe2b668b8af77f965067e08068f965d39d89d
dogmania <hj296914@gmail.com>
2025-10-20 17:58:35 +0900

    ♻️ Refactor: 폴더 경로 변경

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/core/network/di/NetworkModule.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/core/network/di/NetworkModule.kt
@@ -1,4 +1,4 @@
-package com.illdan.desktop.app.di
+package com.illdan.desktop.core.network.di
 
 import com.illdan.desktop.core.network.NetworkClient
 import com.illdan.desktop.core.network.TokenProvider
