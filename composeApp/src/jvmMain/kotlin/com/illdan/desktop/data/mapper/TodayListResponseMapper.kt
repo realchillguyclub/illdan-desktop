4198793c23a7279b6ffffe05bde6b839a219a566
dogmania <hj296914@gmail.com>
2025-10-20 18:02:16 +0900

    ✨ Feat: 오늘 할 일 응답 Mapper 구현

---
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/data/mapper/TodayListResponseMapper.kt
@@ -0,0 +1,16 @@
+package com.illdan.desktop.data.mapper
+
+import com.illdan.desktop.core.network.base.Mapper
+import com.illdan.desktop.data.model.response.today.TodayListResponse
+import com.illdan.desktop.domain.model.response.TodayListInfo
+
+object TodayListResponseMapper: Mapper<TodayListResponse, TodayListInfo> {
+    override fun responseToModel(response: TodayListResponse?): TodayListInfo {
+        return response?.let {
+            TodayListInfo(
+                date = it.date,
+                todays = it.todays.map { todayResponse ->  TodayResponseMapper.responseToModel(todayResponse) }
+            )
+        } ?: TodayListInfo()
+    }
+}
