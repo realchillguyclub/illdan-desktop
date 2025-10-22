a86b41a047b215eb333b326863586b91866aa6da
dogmania <hj296914@gmail.com>
2025-10-22 18:40:13 +0900

    ✨ Feat: data 모듈 초기화

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/app/di/InitKoin.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/app/di/InitKoin.kt
@@ -1,6 +1,9 @@
 package com.illdan.desktop.app.di
 
 import com.illdan.desktop.core.network.di.networkModule
+import com.illdan.desktop.data.di.dataSourceModule
+import com.illdan.desktop.data.di.databaseModule
+import com.illdan.desktop.data.di.driverModule
 import com.illdan.desktop.data.di.repositoryModule
 import com.illdan.desktop.presentation.main.di.mainModule
 import org.koin.core.context.startKoin
@@ -9,6 +12,9 @@ fun initKoin() {
     startKoin {
         modules(
             listOf(
+                driverModule,
+                databaseModule,
+                dataSourceModule,
                 networkModule,
                 repositoryModule,
                 mainModule
