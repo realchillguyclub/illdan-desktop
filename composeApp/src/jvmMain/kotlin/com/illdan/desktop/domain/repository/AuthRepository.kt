53aef834fa928335ea68f0498da8c07431d16ffe
dogmania <hj296914@gmail.com>
2025-10-22 18:41:05 +0900

    ♻️ Refactor: 메서드 주석 처리

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/domain/repository/AuthRepository.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/domain/repository/AuthRepository.kt
@@ -7,8 +7,8 @@ import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
     suspend fun login(request: LoginRequest): Flow<Result<AuthInfo>>
-    suspend fun reissueToken(request: ReissueRequestModel): Flow<Result<AuthTokens>>
-    suspend fun logout(request: LogoutRequestModel): Flow<Result<Unit>>
+//    suspend fun reissueToken(request: ReissueRequestModel): Flow<Result<AuthTokens>>
+//    suspend fun logout(request: LogoutRequestModel): Flow<Result<Unit>>
     suspend fun saveToken(request: AuthTokens): Flow<Result<Unit>>
     suspend fun clearToken(): Flow<Result<Unit>>
     suspend fun getLocalToken(): Flow<AuthTokens>
