341e68249ebdf08d9c89e1dc2ca15b5a42fa51ab
dogmania <hj296914@gmail.com>
2025-10-23 19:46:29 +0900

    🍱 Chore: 리소스 추가

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/core/design_system/String.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/core/design_system/String.kt
@@ -1,3 +1,8 @@
 package com.illdan.desktop.core.design_system
 
+const val WORD_NOTEPAD = "메모장"
+
 const val PLACEHOLDER_TEXT_FILED = "할 일 추가하기..."
+
+const val ALL_TODO = "전체 할 일"
+const val ADD_MEMO = "메모 추가"
