7350a212639460e96fbde5f4cabc3e52e348e737
dogmania <hj296914@gmail.com>
2025-11-14 17:18:51 +0900

    ✨ Feat: AuthViewModel 브라우저 이동 로직 구현

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/presentation/login/AuthViewModel.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/presentation/login/AuthViewModel.kt
@@ -1,11 +1,26 @@
 package com.illdan.desktop.presentation.login
 
+import com.illdan.desktop.BuildKonfig
 import com.illdan.desktop.core.ui.base.BaseViewModel
 import com.illdan.desktop.core.ui.base.UiState
+import com.illdan.desktop.core.util.BrowserManager
+import com.illdan.desktop.core.util.RandomIdGenerator
 import com.illdan.desktop.domain.repository.AuthRepository
 
 class AuthViewModel(
-    private val authRepository: AuthRepository
 ): BaseViewModel<UiState.Default>(UiState.Default) {
-    
+    private val kakaoApiKey = BuildKonfig.KAKAO_API_KEY
+    private val redirectUri = BuildKonfig.REDIRECT_URI
+
+    fun kakaoLogin() {
+        val state = RandomIdGenerator.generateRandomState()
+        val kakaoLoginUrl =
+            "https://kauth.kakao.com/oauth/authorize?" +
+                    "client_id=$kakaoApiKey&" +
+                    "redirect_uri=$redirectUri&" +
+                    "response_type=code&" +
+                    "prompt=login"
+
+        BrowserManager.open(kakaoLoginUrl)
+    }
 }
