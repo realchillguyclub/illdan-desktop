be0ab0e7a9329016458606d80133a5bb3f304b4c
dogmania <hj296914@gmail.com>
2025-11-16 23:33:27 +0900

    ✨ Feat: AuthUrlResponse -> AuthUrl Mapper 구현

---
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/data/mapper/AuthUrlResponseMapper.kt
@@ -0,0 +1,15 @@
+package com.illdan.desktop.data.mapper
+
+import com.illdan.desktop.core.network.base.Mapper
+import com.illdan.desktop.data.model.response.auth.AuthUrlResponse
+import com.illdan.desktop.domain.model.auth.AuthUrl
+
+object AuthUrlResponseMapper: Mapper<AuthUrlResponse, AuthUrl> {
+    override fun responseToModel(response: AuthUrlResponse?): AuthUrl {
+        return response?.let {
+            AuthUrl(
+                it.authorizeUrl
+            )
+        } ?: AuthUrl("")
+    }
+}
