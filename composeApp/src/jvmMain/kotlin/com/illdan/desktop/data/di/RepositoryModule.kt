72516d666795af71d6846abbf813debada466b86
dogmania <hj296914@gmail.com>
2025-11-16 23:33:00 +0900

    ✨ Feat: AuthRepository Koin Module 등록

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/data/di/RepositoryModule.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/data/di/RepositoryModule.kt
@@ -1,9 +1,12 @@
 package com.illdan.desktop.data.di
 
+import com.illdan.desktop.data.repository.AuthRepositoryImpl
 import com.illdan.desktop.data.repository.TodoRepositoryImpl
+import com.illdan.desktop.domain.repository.AuthRepository
 import com.illdan.desktop.domain.repository.TodoRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<TodoRepository> { TodoRepositoryImpl(get(), get()) }
+    single<AuthRepository> { AuthRepositoryImpl(get()) }
 }
