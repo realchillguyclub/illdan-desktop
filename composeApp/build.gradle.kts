70627728b2e79bf6e08f057df8c8188740189444
dogmania <hj296914@gmail.com>
2025-11-20 19:12:09 +0900

    ✨ Feat: java module 추가

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -95,6 +95,7 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "com.illdan.desktop"
             packageVersion = "1.0.0"
+            modules("java.sql")
         }
     }
 }
