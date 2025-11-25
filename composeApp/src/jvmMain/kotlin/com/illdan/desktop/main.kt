138e53ae247c375273f899f0c1a8a453e2ae3219
dogmania <hj296914@gmail.com>
2025-11-25 16:43:05 +0900

    ✨ Feat: Global ImageLoader 적용

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/main.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/main.kt
@@ -2,13 +2,26 @@ package com.illdan.desktop
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import coil3.ImageLoader
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.svg.SvgDecoder
 import com.illdan.desktop.app.di.initKoin
 import com.illdan.desktop.presentation.App
 
+@OptIn(ExperimentalCoilApi::class)
 fun main() {
     initKoin()
 
     application {
+        setSingletonImageLoaderFactory { context ->
+            ImageLoader.Builder(context)
+                .components {
+                    add(SvgDecoder.Factory())
+                }
+                .build()
+        }
+
         Window(
             onCloseRequest = ::exitApplication,
             title = "Illdandesktop",
