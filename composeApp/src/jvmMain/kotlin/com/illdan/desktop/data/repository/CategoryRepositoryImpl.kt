58c4b277ada09c495e270f9c5981d4d037e973c5
dogmania <hj296914@gmail.com>
2025-12-17 13:56:28 +0900

    ✨ Feat: 카테고리 생성 API 통신 구현

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/data/repository/CategoryRepositoryImpl.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/data/repository/CategoryRepositoryImpl.kt
@@ -3,8 +3,11 @@ package com.illdan.desktop.data.repository
 import com.illdan.desktop.core.network.NetworkClient
 import com.illdan.desktop.core.network.base.BaseRepository
 import com.illdan.desktop.data.mapper.CategoryListResponseMapper
+import com.illdan.desktop.data.mapper.GroupEmojiResponseMapper
 import com.illdan.desktop.domain.enums.HttpMethod
 import com.illdan.desktop.domain.model.category.Category
+import com.illdan.desktop.domain.model.category.GroupEmoji
+import com.illdan.desktop.domain.model.request.category.CreateCategoryRequest
 import com.illdan.desktop.domain.repository.CategoryRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -19,6 +22,27 @@ class CategoryRepositoryImpl(
                 path = "/category/list",
                 query = mapOf("page" to "0", "size" to "100"),
                 mapper = CategoryListResponseMapper
+            )
+        )
+    }
+
+    override suspend fun getEmojiList(): Flow<Result<GroupEmoji>> = flow {
+        emit(
+            fetchMapped(
+                method = HttpMethod.GET,
+                path = "/emojis",
+                query = mapOf("page" to "0", "size" to "100"),
+                mapper = GroupEmojiResponseMapper
+            )
+        )
+    }
+
+    override suspend fun createCategory(request: CreateCategoryRequest): Flow<Result<Unit>> = flow {
+        emit(
+            fetch(
+                method = HttpMethod.POST,
+                path = "/category",
+                body = request
             )
         )
     }
