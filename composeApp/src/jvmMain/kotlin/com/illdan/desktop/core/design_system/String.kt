e22b3c9053afa61b287d6c4001db2cf989111178
dogmania <hj296914@gmail.com>
2025-12-17 13:56:56 +0900

    🍱 Chore: String 리소스 추가

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/core/design_system/String.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/core/design_system/String.kt
@@ -3,9 +3,20 @@ package com.illdan.desktop.core.design_system
 const val WORD_NOTEPAD = "메모장"
 const val WORD_BOOKMARK = "중요"
 
+const val ACTION_CLOSE = "닫기"
+const val ACTION_CREATE = "생성하기"
+
 const val PLACEHOLDER_TEXT_FILED = "할 일 추가하기..."
 const val PLACEHOLDER_MEMO_TITLE = "제목을 추가하세요."
 const val PLACEHOLDER_MEMO_CONTENT = "내용을 추가하세요."
+const val PLACEHOLDER_CATEGORY_TEXT_FIELD = "카테고리 이름은 무엇인가요?"
 
 const val ALL_TODO = "전체 할 일"
 const val ADD_MEMO = "메모 추가"
+
+// Category
+const val NEW_CATEGORY = "새 카테고리"
+
+// CategoryDialog
+const val ADD_CATEGORY_DIALOG_TITLE = "카테고리 추가하기"
+const val EDIT_CATEGORY_DIALOG_TITLE = "카테고리 수정하기"
