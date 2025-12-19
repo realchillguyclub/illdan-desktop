97356030b3abd1112f782d96ea9ea82e404a1f8f
dogmania <hj296914@gmail.com>
2025-12-19 16:59:40 +0900

    ✨ Feat: 메모 리스트 조회 API 통신 구현

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/domain/repository/MemoRepository.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/domain/repository/MemoRepository.kt
@@ -1,9 +1,11 @@
 package com.illdan.desktop.domain.repository
 
+import com.illdan.desktop.domain.model.memo.Memo
 import com.illdan.desktop.domain.model.memo.MemoId
 import com.illdan.desktop.domain.model.request.memo.SaveMemoRequest
 import kotlinx.coroutines.flow.Flow
 
 interface MemoRepository {
     suspend fun saveMemo(request: SaveMemoRequest): Flow<Result<MemoId>>
+    suspend fun getMemoList(): Flow<Result<List<Memo>>>
 }
