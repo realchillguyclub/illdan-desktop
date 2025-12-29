b32c6afa04192d1b21b0f8dd767d8f202a96cf30
dogmania <hj296914@gmail.com>
2025-12-30 02:16:46 +0900

    ✨ Feat: UnknownUser Domain Error 추가

--- composeApp/src/jvmMain/kotlin/com/illdan/desktop/domain/error/DomainError.kt
+++ composeApp/src/jvmMain/kotlin/com/illdan/desktop/domain/error/DomainError.kt
@@ -4,6 +4,7 @@ import okio.IOException
 
 sealed class DomainError: Throwable() {
     data object AuthExpired: DomainError()
+    data object UnKnownUser: DomainError()
     data class Network(val e: IOException? = null): DomainError()
     data class Unknown(val e: Throwable? = null): DomainError()
 }
